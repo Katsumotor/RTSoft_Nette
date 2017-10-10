326f0f1fd446bfdbdc48e3af6e0736e745145c07
lang <>
2017-10-10 14:17:58 +0200

    rosireni

--- app/forms/ProjectForm.php
+++ app/forms/ProjectForm.php
@@ -39,52 +39,48 @@ class ProjectForm extends Nette\Application\UI\Control {
         $users = $form->addDynamic('users', function (Container $user) {
             $user->addSelect('name', 'Zadejte účastníka na projektu', $this->queryProjects->getAllPerson())
                     ->setPrompt("")
-                    ->setRequired()
-                    ->addRule(\App\Model\Validator::ISALREADYSELECTPERSON,'Tento pracovník je jíž v seznamu',$this['projectForm']['users']->getValues());
-            $user->addSubmit('remove', 'Odebrat pracovníka')
-                    ->addRemoveOnClick();
-        }, 0);
-
-        $users->addSubmit('add', 'Přidat pracovníka')
-                ->addCreateOnClick();
+                    ->setRequired(false)
+                    ->addRule(\App\Model\Validator::ISALREADYSELECTPERSON, 'Tento pracovník je jíž v seznamu', array($this['projectForm']['users']->getValues(), $user->values));
+        }, $this->queryProjects->getCountPersonInDB());
         $form->addSubmit('vlozProjekt', 'Vlozit projekt');
         $form->onSuccess[] = array($this, 'checkProjectForm');
         return $form;
     }
 
-    public function checkProjectForm($form, $values) {
+    public function checkProjectForm(Nette\Application\UI\Form $form, \Nette\Utils\ArrayHash $values) {
         $this->onProjectForm($this, $values);
     }
 
     public function loadDataIntoForm() {
         $row = $this->queryProjects->getProjectById($this->id);
-       if($row){
-        $form = $this['projectForm'];
-        if (!$form->isSubmitted()) {
-            $form->setValues([
-                'nazevProjektu' => $row->NazevProjektu,
-                'datumOdevzdaniProjektu' => Nette\Utils\DateTime::From($row->DatumOdevzdaniProjektu)->format('d.m.Y'),
-                'typprojektu' => $row->TypProjektu,
-                'webovyprojekt' => $row->WebovyProjekt
-            ]);
-            foreach ($row->related('persononproject') as $person) {
-                $form['users'][$person->id_person]->setValues(['name' => $person->id_person]);
+        if ($row) {
+            $form = $this['projectForm'];
+            if (!$form->isSubmitted()) {
+                $form->setValues([
+                    'nazevProjektu' => $row->NazevProjektu,
+                    'datumOdevzdaniProjektu' => Nette\Utils\DateTime::From($row->DatumOdevzdaniProjektu)->format('d.m.Y'),
+                    'typprojektu' => $row->TypProjektu,
+                    'webovyprojekt' => $row->WebovyProjekt
+                ]);
+                $i = 0;
+                foreach ($row->related('persononproject') as $person) {
+                    $form['users'][$i]->setValues(['name' => $person->id_person]);
+                    $i++;
+                }
             }
+            return true;
         }
-        return true;
-       }
-       return false;
+        return false;
     }
 
     public function render() {
         if ($this->id != null) {
-            if($this->loadDataIntoForm()){
-              $this['projectForm']->render();   
-            }       
-        }else{
-               $this['projectForm']->render();   
+            if ($this->loadDataIntoForm()) {
+                $this['projectForm']->render();
+            }
+        } else {
+            $this['projectForm']->render();
         }
-   
     }
 
 }
